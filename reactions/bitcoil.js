917d3563578f64905771ada31b7d6c82edf63a75
taylor <rhodes4ao@gmail.com>
2020-01-19 00:00:08 -0800

    bc

--- reactions/bitcoil.js
+++ reactions/bitcoil.js
@@ -11,24 +11,6 @@ const pin6 = new Gpio(6, 'out')
 const pin7 = new Gpio(7, 'out')
 const pin8 = new Gpio(8, 'out')
 const pin9 = new Gpio(9, 'out')
-const pin10 = new Gpio(10, 'out')
-const pin11 = new Gpio(11, 'out')
-const pin12 = new Gpio(12, 'out')
-const pin13 = new Gpio(13, 'out')
-const pin14 = new Gpio(14, 'out')
-const pin15 = new Gpio(15, 'out')
-const pin16 = new Gpio(16, 'out')
-
-const pin17 = new Gpio(17, 'in', 'both')
-const pin18 = new Gpio(18, 'in', 'both')
-const pin19 = new Gpio(19, 'in', 'both')
-const pin20 = new Gpio(20, 'in', 'both')
-const pin21 = new Gpio(21, 'in', 'both')
-const pin22 = new Gpio(22, 'in', 'both')
-const pin23 = new Gpio(23, 'in', 'both')
-
-const pin24 = new Gpio(24, 'out')
-const pin25 = new Gpio(25, 'out')
 
 pin2.writeSync(1)
 pin3.writeSync(1)
@@ -38,15 +20,6 @@ pin6.writeSync(1)
 pin7.writeSync(1)
 pin8.writeSync(1)
 pin9.writeSync(1)
-pin10.writeSync(1)
-pin11.writeSync(1)
-pin12.writeSync(1)
-pin13.writeSync(1)
-pin14.writeSync(1)
-pin15.writeSync(1)
-pin16.writeSync(1)
-pin24.writeSync(1)
-pin25.writeSync(1)
 
 var emit
 var dispenseStream = Kefir.stream(emitter => {
@@ -71,23 +44,50 @@ function highLow(pin){
 function vend(usedEv){
     console.log("vending:", usedEv)
     switch (usedEv.notes){
-        case 'A':
+        case 'A1':
             highLow(pin4)
             break
-        case 'B':
+        case 'A2':
             highLow(pin5)
             break
-        case 'C':
+        case 'A3':
             highLow(pin6)
             break
-        case 'D':
-            highLow(pin12)
+        case 'B1':
+            highLow(pin2)
+            highLow(pin4)
             break
-        case 'E':
-            highLow(pin13)
+        case 'B2':
+            highLow(pin2)
+            highLow(pin5)
             break
-        case 'F':
-            highLow(pin16)
+        case 'B3':
+            highLow(pin2)
+            highLow(pin6)
+            break
+        case 'C1':
+            highLow(pin3)
+            highLow(pin4)
+            break
+        case 'C2':
+            highLow(pin3)
+            highLow(pin5)
+            break
+        case 'C3':
+            highLow(pin3)
+            highLow(pin6)
+            break
+        case 'C4':
+            highLow(pin3)
+            highLow(pin7)
+            break
+        case 'C5':
+            highLow(pin3)
+            highLow(pin8)
+            break
+        case 'C6':
+            highLow(pin3)
+            highLow(pin9)
             break
     }
 }
